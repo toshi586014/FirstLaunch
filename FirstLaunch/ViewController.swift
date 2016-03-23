3a5f56158d17c48cc5310ce5229c2df2d93a44dc
toshi586014 <toshi586014@icloud.com>
2016-03-23 17:51:40 +0900

    Initial Commit

---
+++ FirstLaunch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstLaunch
+//
+//  Created by Toshiaki Nakamura on 2016/03/23.
+//  Copyright © 2016年 Toshiaki Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
